39cdc753c29e276cb764750a97141ab769ef2e08
EneasCarranza <76787517+EneasCarranza@users.noreply.github.com>
2021-03-24 18:28:00 -0300

    master

--- ejemplos_clase.py
+++ ejemplos_clase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+\#!/usr/bin/env python
 '''
 Condicionales [Python]
 Ejemplos de clase
---
+++ mi practica .py
@@ -0,0 +1,4 @@
+    if numero_1 > numero_2:
+        print("el munero", numero_1 ,"es mayor que el ", numero_2)
+    else:
+        print("el munero", numero_2, "es mayor que el ", numero_1)
