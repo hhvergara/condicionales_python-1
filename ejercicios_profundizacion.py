181d56153c84547b652b6a60fe1ef69375a74744
EneasCarranza <76787517+EneasCarranza@users.noreply.github.com>
2021-04-03 18:12:49 -0300

    Update ejercicios_profundizacion.py

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -23,6 +23,19 @@ def ej1():
     Calcule la diferencia entre ellos e informe por pantalla
     si el resultado es positivo, negativo o cero.
     '''
+print("ingrese el primer numero")
+primer = int(input())
+print("ingrese el segundo numero")
+segundo = int(input())
+resultado= primer-segundo
+print("EL RESULTADO ES: ",resultado)
+if resultado>0:
+    print("Es positivo el numero: ",resultado)
+elif resultado<0:
+    print("Es negativo el numero: ",resultado)
+else:
+    print("el numero que ingresaste es 0")
+
 
 
 def ej2():
@@ -34,6 +47,26 @@ def ej2():
     o impar.
     Para cada caso imprimir el resultado en pantalla.
     '''
+print("inserte el numero 1 ")
+primer= int(input())
+print("inserte el numero 2 ")
+segundo= int(input())
+print("inserte el numero 3 ")
+tercer= int(input())
+
+
+if (primer % 2) == 0:
+    print("el primer numero es par")
+else:
+    print("el primer numero es IMPAR")
+if (segundo % 2) == 0:    
+    print("el segundo numero es par")
+else:     
+    print("el segundo numero es impar")
+if (tercer % 2) == 0:    
+    print("el tercer numero es par")
+else:   
+    print("el tercer numero es impar")
 
 
 def ej3():
@@ -52,7 +85,27 @@ def ej3():
     Se debe efectuar el cálculo correcto según la operación ingresada por consola
     Imprimir en pantalla la operación realizada y el resultado
     '''
-
+print("ingrese el numero 1 ")
+primer= int(input())
+print("ingrese el numero 2 ")
+segundo= int(input())
+print(""ahora ingrese alguno de estas operaciones: 
+Suma (+)
+Resta (-)
+Multiplicación (*)
+División (/)
+Exponente/Potencia (**)""")
+operacion = str(input())    
+if operacion == "+":
+    print("la suma del primer numero y el segundo numero da:", primer + segundo)
+elif operacion == "-":
+    print("la resta del primer numero y el segundo numero da:", primer - segundo)
+elif operacion == "*":    
+    print("la multiplicacion del primer numero y el segundo numero da:", primer * segundo)
+elif operacion == "/":
+    print("la division del primer numero y el segundo numero da:", primer / segundo)
+else operacion == "**":
+    print("La exponencianzion del primer numero y el segundo numero da:", primer ** segundo)
 
 def ej4():
     print('Ejercicios de práctica con cadenas')
@@ -69,7 +122,24 @@ def ej4():
     Si se ingresa "2" por consola se deben ordenar las 3 palabras por cantidad de letras
     e imprimir en pantalla de la mayor a la menor
     '''
+print("ingrese la palabra 1") 
+primer= str(input())
+print("ingrese la palabra 2") 
+segunda= str(input())
+print("ingrese la palabra 3") 
+tercera= str(input())
+print(""" elija entre estas opciones tocando 1 o 2:
+    1 - Ordenar alfabétticamente.
+    2 - (Longitud de palabra)""")
+eleccion= int(input())
+juntas= primer,segunda,tercera
 
+if eleccion ==1:
+    print(sorted(juntas))
+elif eleccion==2:
+    print(sorted(juntas,key = len))
+else: 
+    print("no eligio nunguna de ellas")
 
 def ej5():
     print('Ejercicios de práctica con números')
@@ -83,6 +153,17 @@ def ej5():
 
     En cada caso imprimir en pantalla el resultado
     '''
+print("ingrese cantidad de temperatura 1")
+valor_1= float(input())
+print("ingrese cantidad de temperatura 2")
+valor_2= float(input())
+print("ingrese cantidad de temperatura 3")
+valor_3= float(input())
+total= cantidad_1,cantidad_2,cantidad_3 
+print("el maximo es:", max(total))
+print("el minimo es:", min(total))
+promedio= (cantidad_1+cantidad_2+cantidad_3) /3
+print("el promedio es:", promedio)
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
